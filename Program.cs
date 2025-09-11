c318dbc874d64d77581d444ecddee90aebd8fc42
Board Collie <cardchess.leader@gmail.com>
2025-09-11 15:20:57 +0900

    FlipArray

--- Program.cs
+++ Program.cs
@@ -1,33 +1,37 @@
-﻿using System.Collections.Specialized;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        int[] a = { 15, 3, 9, 27, -5, 8, 99 };
-        int max = int.MinValue, min = int.MaxValue;
-
-        foreach(var num in a)
+        int[,] map =
         {
-            if(num > max) max = num;
-            if(num < min) min = num;
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 },
+        };
+        Flip2DArray(map);
+        for(int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                Console.Write(map[i, j]);
+            }
+            Console.WriteLine();
         }
-
-        Console.WriteLine(max);
-        Console.WriteLine(min);
-        CountingStar("abczzz");
     }
 
-    static void CountingStar(string str)
+    static void Flip2DArray(int[,] a)
     {
-        var arr = new int[26];
-        foreach(var c  in str)
+        for (int i = 0; i < a.GetLength(0); i++)
         {
-            arr[c - 'a']++;
-        }
-        foreach(var i in arr)
-        {
-            Console.Write(i + " ");
+            for (int j = 0; j <  a.GetLength(1); j++)
+            {
+                if (i > j)
+                {
+                    int temp = a[i, j];
+                    a[i, j] = a[j, i];
+                    a[j, i] = temp;
+                }
+            }
         }
     }
 }
