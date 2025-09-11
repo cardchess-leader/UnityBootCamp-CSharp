69e4c643e92ef5f2872ca1accc28d473d71a78e4
Board Collie <cardchess.leader@gmail.com>
2025-09-11 09:39:51 +0900

    Ref

--- Program.cs
+++ Program.cs
@@ -1,175 +1,19 @@
-﻿class CustomList
+﻿class Program
 {
-    private int[] items;
-    public int count;
-    public CustomList()
+    static void Swap(ref int a, ref int b)
     {
-        items = new int[1];
-        count = 0;
-    }
-    public void Add(int item)
-    {
-        if (count == items.Length)
-        {
-            Resize();
-        }
-        items[count] = item;
-        count++;
-    }
-    public bool Remove(int index)
-    {
-        for (int i = index; i < count - 1; i++)
-        {
-            items[i] = items[i + 1];
-        }
-        count--;
-        return true;
-    }
-    public int Count
-    {
-        get { return count; }
-    }
-    private void Resize()
-    {
-        int newSize = items.Length * 2;
-        int[] newItems = new int[newSize];
-        for (int i = 0; i < items.Length; i++)
-        {
-            newItems[i] = items[i];
-        }
-        items = newItems;
+        int temp = a;
+        a = b;
+        b = temp;
     }
 
-    public void PrintList()
-    {
-        for(int i = 0; i < count; i++)
-        {
-            Console.WriteLine("Value is: " + items[i]);
-        }
-    }
-}
-class Node
-{
-    public int Value;
-    public Node? Next;
-    public Node? Prev;
-    public Node(int value)
-    {
-        Value = value;
-        Next = null;
-        Prev = null;
-    }
-}
-
-class CustomLinkedList {     
-    private Node? head;
-    private Node? tail;
-    private int count;
-    
-    public CustomLinkedList()
-    {
-        head = null;
-        tail = null;
-        count = 0;
-    }
-    
-    public Node Add(int item)
-    {
-        Node newNode = new Node(item);
-        
-        if (head == null)
-        {
-            head = newNode;
-            tail = newNode;
-        }
-        else
-        {
-            tail!.Next = newNode;
-            newNode.Prev = tail;
-            tail = newNode;
-        }
-        count++;
-        return newNode;
-    }
-    
-    public bool Remove(Node node)
-    {
-        if (head == null)
-        {
-            return false;
-        }
-        
-        Node? current = head;
-        
-        while (current != null && current != node)
-        {
-            current = current.Next;
-        }
-        
-        if (current == null)
-        {
-            return false;
-        }
-        
-        if (current.Prev != null)
-        {
-            current.Prev.Next = current.Next;
-        }
-        else
-        {
-            head = current.Next;
-        }
-        
-        if (current.Next != null)
-        {
-            current.Next.Prev = current.Prev;
-        }
-        else
-        {
-            tail = current.Prev;
-        }
-        
-        if (count == 1)
-        {
-            head = null;
-            tail = null;
-        }
-        
-        count--;
-        return true;
-    }
-    
-    public int Count
-    {
-        get { return count; }
-    }
-}
-
-class Program
-{
     static void Main(string[] args)
     {
-        CustomList customList = new();
-        customList.Add(1);
-        customList.Add(2);
-        customList.Add(3);
-        customList.Remove(1);
-        customList.PrintList();
+        int x = 10;
+        int y = 20;
+        Swap(ref x, ref y);
 
-        CustomLinkedList list = new CustomLinkedList();
-        var firstNode = list.Add(1);
-        var secondNode = list.Add(2);
-        list.Add(3);
-        var thirdNode = list.Add(4);
-        list.Add(5);
-
-        list.Remove(secondNode);
-        list.Remove(thirdNode);
-
-        while(firstNode != null)
-        {
-            Console.WriteLine("Node value is: " + firstNode.Value);
-            firstNode = firstNode.Next;
-        }
+        Console.WriteLine(x);
+        Console.WriteLine(y);
     }
 }
--- obj/Debug/net9.0/ConsoleApp12.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp12.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a92ece62bbe815c529e73674909ed60dec0e9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
